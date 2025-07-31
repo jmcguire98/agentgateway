a76dc881b1c243d672f61651e647b040fa33b451
Joe McGuire <joe.alter.mcguire@gmail.com>
2025-07-31 12:06:17 -0600

    clean up next config since envvar will not work

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/ui",
   trailingSlash: true,
-  env: {
-    XDS_ADDRESS: process.env.XDS_ADDRESS,
-  },
 };
 
 export default nextConfig;
